f810c4445f725aefbea2de21bc9cdc4ec170e8f8
BivasBiswas <bivasbiswas1999@gmail.com>
2021-08-31 00:16:52 +0530

    :art: args in v6

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -3,10 +3,29 @@ import Button from "./Button";
 
 export default {
     title: 'Form/Button',
-    component: Button
+    component: Button,
+    args:{
+        children: 'Button'
+    }
 }
 
 export const Primary = () => <Button variant={'primary'}>Primary</Button>
 export const Secondary = () => <Button variant={'secondary'}>Secondary</Button>
 export const Succes = () => <Button variant={'success'}>Succes</Button>
 export const Danger = () => <Button variant={'danger'}>Danger</Button>
+
+const Teplate = arg => <Button {...arg}/>
+
+export const PrimaryA = Teplate.bind({})
+
+PrimaryA.args = {
+    variant: 'primary',
+    children:'Primary Args'
+}
+
+export const SecondaryA = Teplate.bind({})
+
+SecondaryA.args = {
+    variant: 'secondary',
+    children:'Secondary Args'
+}
