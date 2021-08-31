356e7c78c0593bc931b51d7c36c58de66710a88c
BivasBiswas <bivasbiswas1999@gmail.com>
2021-08-31 12:10:08 +0530

    :art: themeing

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,7 @@
 import { addDecorator } from "@storybook/react";
-import Center from "../src/components/Center/Center";
+// import Center from "../src/components/Center/Center";
+import { ThemeProvider, CSSReset, theme } from "@chakra-ui/react";
+import React from "react";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -16,4 +18,5 @@ export const parameters = {
 }
 
 // global decorators
-addDecorator(story => <Center>{story()}</Center>)
+// addDecorator(story => <Center>{story()}</Center>)
+addDecorator(story => <ThemeProvider theme={theme}><CSSReset/>{story()}</ThemeProvider>)
